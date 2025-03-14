81b29d85e3bc7b997ba7dd620a561f0a879e97b9
Etienne Maire <layzy37@gmail.com>
2025-03-14 12:47:24 +0100

    Changed the project structure

--- GAME/game.py
+++ GAME/game.py
@@ -3,7 +3,7 @@ import math
 import sys
 # from GAME.defines import * # Ne fonctionne pas car ca duplique les variables
 import GAME.defines
-from GAME.renderer import normalize_vector2d
+from GAME.renderer import normalize_vector2d, Renderer
 from GAME.rays import cast_ray
 import GAME.map as mp
 
@@ -154,3 +154,27 @@ class Game:
         pygame.display.update()
 
         self.loading = False
+
+    def run(self):
+        infoObject = pygame.display.Info()
+        infoObject = pygame.display.Info()
+        SCREEN_WIDTH = infoObject.current_w
+        SCREEN_HEIGHT = infoObject.current_h
+        RESOLUTION_X = GAME.defines.RESOLUTION_X
+        RESOLUTION_Y = GAME.defines.RESOLUTION_Y
+        if GAME.defines.FULL_RES:
+            RESOLUTION_X = SCREEN_WIDTH
+            RESOLUTION_Y = SCREEN_HEIGHT
+        renderer = Renderer(RESOLUTION_X, RESOLUTION_Y)
+
+        # print(maze_to_map(32, 32))
+
+        map1 = mp.Map()
+        map1.load_from_list(GAME.defines.MAP1, GAME.defines.MAP1_INTERACT, GAME.defines.MAP1_SIZE_X, GAME.defines.MAP1_SIZE_Y,
+                            ["RESOURCES/pack/TILE_2C.PNG", "RESOURCES/082.png", "RESOURCES/pack/TECH_1C.PNG", "RESOURCES/pack/TECH_1E.PNG", "RESOURCES/pack/TECH_2F.PNG", "RESOURCES/pack/CONSOLE_1B.PNG", "RESOURCES/pack/TECH_3B.PNG", "RESOURCES/pack/SUPPORT_4A.PNG"], 0, 1)
+
+        while True:
+            self.handleEvents()
+            self.update(map1)
+            renderer.update(self.in_menu, map1, self.player_x, self.player_y, self.player_z, self.player_angle)
+            self.display(renderer.buffer)
--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-import pygame
-import cProfile
 from GAME.game import Game
-from GAME.renderer import Renderer
-from GAME.map import Map
-# from GAME.defines import *
 import GAME.defines
 
 def main():
     game = Game(GAME.defines.MAP1_SIZE_X / 2, GAME.defines.MAP1_SIZE_Y / 2 + 1)
-    infoObject = pygame.display.Info()
-    infoObject = pygame.display.Info()
-    SCREEN_WIDTH = infoObject.current_w
-    SCREEN_HEIGHT = infoObject.current_h
-    RESOLUTION_X = GAME.defines.RESOLUTION_X
-    RESOLUTION_Y = GAME.defines.RESOLUTION_Y
-    if GAME.defines.FULL_RES:
-        RESOLUTION_X = SCREEN_WIDTH
-        RESOLUTION_Y = SCREEN_HEIGHT
-    renderer = Renderer(RESOLUTION_X, RESOLUTION_Y)
-
-    # print(maze_to_map(32, 32))
-
-    map1 = Map()
-    map1.load_from_list(GAME.defines.MAP1, GAME.defines.MAP1_INTERACT, GAME.defines.MAP1_SIZE_X, GAME.defines.MAP1_SIZE_Y,
-                        ["RESOURCES/pack/TILE_2C.PNG", "RESOURCES/082.png", "RESOURCES/pack/TECH_1C.PNG", "RESOURCES/pack/TECH_1E.PNG", "RESOURCES/pack/TECH_2F.PNG", "RESOURCES/pack/CONSOLE_1B.PNG", "RESOURCES/pack/TECH_3B.PNG", "RESOURCES/pack/SUPPORT_4A.PNG"], 0, 1)
-
-    while True:
-        game.handleEvents()
-        game.update(map1)
-        renderer.update(game.in_menu, map1, game.player_x, game.player_y, game.player_z, game.player_angle)
-        game.display(renderer.buffer)
+    game.run()
 
 if __name__ == "__main__": # Pour profiler : python3 -m cProfile -s tottime main.py | head -n 35
     main()
