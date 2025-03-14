1654f8c372b636eda4de0dcc35fb8479eb23dae6
Etienne Maire <layzy37@gmail.com>
2025-03-14 19:41:37 +0100

    Added more textures and started working on entity support

---
+++ GAME/entity.py
@@ -0,0 +1,15 @@
+import pygame
+import numpy
+import os
+import random 
+import math
+
+class Entity:
+    def __init__(self, pos_x: float, pos_y: float, pos_z: float, size_x: float, size_y: float, tex_file: str, alpha: tuple):
+        self.position = (pos_x, pos_y, pos_z)
+        self.size = (size_x, size_y)
+        tex = pygame.image.load(os.path.abspath(tex_file))
+        self.texture = pygame.surfarray.array3d(tex).astype(numpy.uint8)
+        self.texture_size = tex.get_size()
+        self.alpha_color = alpha
+        self.attack_timer = 1
